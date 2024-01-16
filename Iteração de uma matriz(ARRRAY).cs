27daa82542746605becae8f59a546b0400642f76
Nicolly95 <nicollykingeski@hotmail.com>
2024-01-16 09:11:12 -0300

    Subindo o código

---
+++ Iteração de uma matriz(ARRRAY).cs
@@ -0,0 +1,9 @@
+﻿//Iteração de uma matriz (array)
+
+string[] minhaString = new string[4] {"I like pizza.", "I like roast chicken.", "I like salad", "I like all three of the menu choices"};
+
+foreach (string menu in minhaString)
+{
+    Console.WriteLine(menu);
+}
+
